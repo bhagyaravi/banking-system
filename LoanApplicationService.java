be80e6517af89642c7b1250f64a08ae201c56da8
bhagyaravi <bhagyaravi98@gmail.com>
2025-09-28 13:25:17 +0530

    initial commit

---
+++ LoanApplicationService.java
@@ -0,0 +1,11 @@
+package com.example.loanapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanApplicationService {
+
+    public String initiateLoanApplication() {
+        return "Loan application initiated";
+    }
+}
